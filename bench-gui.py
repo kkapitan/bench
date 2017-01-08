cea5091d60bcab6a6a76fe032c667e6e1a386e55
Krzysztof <krzysztof.kapitan@gmail.com>
2017-01-08 17:17:44 +0100

    V 0.99999

--- bench-gui.py
+++ bench-gui.py
@@ -1,11 +1,9 @@
 import Tkinter, Tkconstants, tkFileDialog
 from Tkinter import *
 
-import os, csv
-
-import matplotlib
+import os, csv, matplotlib
 matplotlib.use('TkAgg')
-from numpy import arange, sin, pi
+
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
@@ -14,12 +12,27 @@ import subprocess32
 
 class DirectoryPickerView(Tkinter.Frame):
 
-  def __init__(self, root, name):
+  def __init__(self, root, name, show_checkbox=True):
     Tkinter.Frame.__init__(self, root)
     self.directory = StringVar(None)
 
     self.setupEntry()
-    self.setupButton(name, self.askDirectory)
+    button = self.setupButton(name, self.askDirectory)
+
+    if show_checkbox:
+      self.checkState = IntVar(None)
+      self.setupCheckbox(self.checkState, button)
+
+  def setupCheckbox(self, stateVar, widget):
+    def cb():
+      if self.checkState.get() == 0:
+        widget.config(state="disabled")
+        self.directory.set("")
+      else:
+        widget.config(state="normal")
+
+    Checkbutton(root, text="Add expected output", variable=stateVar, onvalue=1, offvalue=0, command=cb).pack()
+    cb()
 
   def setupEntry(self):
     entry_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
@@ -31,7 +44,9 @@ class DirectoryPickerView(Tkinter.Frame):
   def setupButton(self, text, command):
     button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
 
-    Tkinter.Button(self, text=text, command=command).pack(**button_opt)
+    button = Tkinter.Button(self, text=text, command=command)
+    button.pack(**button_opt)
+    return button
 
   def askDirectory(self):
     options = {'initialdir': '~', 'mustexist': False,'parent':root,'title':'This is a title'}
@@ -42,7 +57,7 @@ class FilePickerView(Tkinter.Frame):
 
   def __init__(self, root, name):
     Tkinter.Frame.__init__(self, root)
-    self.fileName = StringVar(None)
+    self.fileName = StringVar(0)
 
     self.setupEntry()
     self.setupButton(name, self.askFilename)
@@ -62,6 +77,39 @@ class FilePickerView(Tkinter.Frame):
   def askFilename(self):
     options = {'filetypes' : [('all files', '.*'), ('text files', '.txt')], 'initialdir' : '~', 'parent': root, 'title' : 'This is a title'}
     self.fileName.set(tkFileDialog.askopenfilename(**options))
+
+
+
+class ArgumentsView(Tkinter.Frame):
+
+  def __init__(self, root):
+    Tkinter.Frame.__init__(self, root)
+    self.arguments = StringVar(None)
+
+    entry = self.setupEntry()
+
+    self.checkState = IntVar(None)
+    self.setupCheckbox(self.checkState, entry)
+
+  def setupCheckbox(self, stateVar, widget):
+    def cb():
+      if self.checkState.get() == 0:
+        widget.config(state="disabled")
+        self.arguments.set("")
+      else:
+        widget.config(state="normal")
+
+    Checkbutton(root, text="Add command line arguments", variable=stateVar, onvalue=1, offvalue=0, command=cb).pack()
+    cb()
+
+  def setupEntry(self):
+    entry_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
+
+    entry = Entry(root, textvariable=self.arguments)
+    entry.pack(**entry_opt)
+    entry.configure(state="disabled")
+
+    return entry
 
 class TkPlotCanvas(Tkinter.Frame):
   def __init__(self, root):
@@ -110,8 +158,11 @@ class MainView(Tkinter.Frame):
     self.executablePicker = FilePickerView(root, "Choose executable")
     self.executablePicker.pack()
 
-    self.inputPicker = DirectoryPickerView(root, "Choose input directory")
+    self.inputPicker = DirectoryPickerView(root, "Choose input directory", False)
     self.inputPicker.pack()
+
+    self.argumentsView = ArgumentsView(root)
+    self.argumentsView.pack()
 
     self.outputPicker = DirectoryPickerView(root, "Choose output directory")
     self.outputPicker.pack()
@@ -119,9 +170,13 @@ class MainView(Tkinter.Frame):
     self.executablePicker.fileName.set("ls")
     self.inputPicker.directory.set("/Users/kkapitan/bench/test")
 
+    self.setupButton()
+
     self.plotCanvas = TkPlotCanvas(root)
     self.plotCanvas.place()
 
+
+  def setupButton(self):
     button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
     Tkinter.Button(root, text="Bench it!", command=self.main_action).pack(**button_opt)
 
@@ -129,6 +184,7 @@ class MainView(Tkinter.Frame):
 
     inDir = self.inputPicker.directory.get()
     outDir = self.outputPicker.directory.get()
+    args = self.argumentsView.arguments.get()
     cmd = self.executablePicker.fileName.get()
 
     pythonPath = "/usr/bin/python"
@@ -137,6 +193,8 @@ class MainView(Tkinter.Frame):
     if outDir != None and len(outDir) > 0:
       benchCommand = benchCommand + ' --output ' + outDir
     benchCommand = benchCommand + ' ' + cmd
+    if args != None and len(args) > 0:
+      benchCommand = benchCommand + ' ' + args
 
     benchCommandArray = (pythonPath + ' ' + benchPath + benchCommand).split(" ")
 
@@ -144,6 +202,8 @@ class MainView(Tkinter.Frame):
 
     csvfile = open('res.csv', 'rb')
     reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+
+    print benchCommandArray
 
     testNames = []
     testTimes = []
@@ -162,6 +222,6 @@ if __name__=='__main__':
   MainView(root).place()
   root.title("Bench by Krzysztof Kapitan & Jan Badura")
   root.resizable(0, 0)
-  root.geometry("500x800")
+  root.geometry("500x900")
 
   root.mainloop()
